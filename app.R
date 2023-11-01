2c07eb28c80d45511335564e742d88c901be8ad2
Roberto Sanchez <robertosan97@gmail.com>
2023-11-01 19:08:36 +0100

    add inputs for group level, variable to plot, bootstraps and datapoints

--- app.R
+++ app.R
@@ -23,16 +23,30 @@ download_data()
 metadata <- read_metadata("data/bac120_metadata_r214.tsv.gz")
 tree <- read_and_format_tree("data/bac120_r214.tree", metadata = metadata)
 
-
-
 # Define UI for the Shiny app
 ui <- fluidPage(
   titlePanel("Select a taxon"),
   
   sidebarLayout(
+    
     sidebarPanel(
+      
       selectInput("subset_level", "Select a level:", choices = tax_levels),
+      
       selectizeInput("subset_taxon", "Select a taxon:", choices = NULL), 
+      
+      selectInput("group_level", "Select a level to group", choices = NULL),
+      
+      selectInput(
+        "variable_x",
+        "Select a variable to plot",
+        choices = c("gc_percentage", "genome_size", "coding_density")),
+      
+      # Checkbox to show bootstraps
+      checkboxInput("show_bootstraps", "Show bootstraps", value = FALSE),
+      
+      checkboxInput("show_datapoints", "Show individual datapoints", value = FALSE),
+      
       actionButton("show", "Show tree"),
     ),
     mainPanel(
@@ -46,31 +60,66 @@ server <- function(input, output, session) {
   
   # Update the selectize input choices based on the selected level
   observe({
-    choices <- switch(
+    taxa_choices <- switch(
       input$subset_level, 
       get_all_taxa(metadata)[[input$subset_level]]
       )
     
-    updateSelectizeInput(session, "subset_taxon", choices = choices, server = TRUE)
+    # Update the selectize with the corresponding choices
+    # TODO:, if you use named vectors, a label different from the value 
+    # is displayed. Use this to display something like 
+    # Taxon_a ({n_genome} genomes)
+    
+    updateSelectizeInput(
+      session, 
+      "subset_taxon",
+      choices=taxa_choices, 
+      server=TRUE
+      )
+    
+    updateSelectInput(
+      session, 
+      "group_level",
+      # Only levels lower than the subset level are allowed
+      choices=tax_levels[(which(tax_levels == input$subset_level) + 1):7]
+      )
+    
   })
   
-  # One the action button is pressed, calculate the subset_tree
+  # Once the action button is pressed, calculate the subset_tree
   
   subset_tree <- eventReactive(input$show, {
     subset_tree_to_taxon(tree, input$subset_level, input$subset_taxon)
   })
   
   
-  # Display the selected text from the selectize input
+  # Display the girafe plot
   output$tree <- renderGirafe({
     
     tree_interactive <- subset_tree() |>
       ggtree() |> 
-      scale_tree_to_level("genus") |> 
-      annotate_taxon_nodes("genus", metadata_df = metadata) |> 
-      collapse_tree_to_level("genus") 
+      scale_tree_to_level(input$group_level) |> 
+      annotate_taxon_nodes(input$group_level, metadata_df = metadata) |> 
+      collapse_tree_to_level(input$group_level) |> 
+      add_tips()
     
-    girafe(ggobj = tree_interactive)
+    if (input$show_bootstraps) {
+      tree_interactive <- add_bootstraps(tree_interactive)
+    }
+    
+      boxplot_interactive <- plot_stat_boxplot(
+        tree_interactive, 
+        stat = input$variable_x,
+        metadata = metadata,
+        level = input$group_level,
+        show_datapoints = input$show_datapoints 
+      )
+      
+      girafe(
+        ggobj = plot_tree_and_boxplots(
+          tree_interactive,
+          boxplot_interactive)
+      )
   })
 
 }
