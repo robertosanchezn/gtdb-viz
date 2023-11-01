7d2407de0a39f0d4db34c8f86c35ed86cb4ee7f7
Roberto Sanchez <robertosan97@gmail.com>
2023-11-01 09:07:24 +0100

    subset_tree_to_taxon takes the prefixed taxon name

--- scripts/wrangle_tree.R
+++ scripts/wrangle_tree.R
@@ -1,8 +1,8 @@
-subset_tree_to_taxon <- function(tree = gtdb_tree, level, taxon) {
-  letter <- str_sub(level, 1, 1)
-  pattern <-paste0(letter, "__", taxon, "(?:;|$)") 
+subset_tree_to_taxon <- function(tree = gtdb_tree, suffixed_taxon) {
   
-  node <- as_tibble(tree) |>
+  pattern <-paste0(suffixed_taxon, "(?:;|$)") 
+  
+  node <- tibble(as_tibble(tree)) |>
     filter(str_detect(taxon, pattern)) |>
     pull(node)
   
